b5556b289fe307fbf973f72b928931478865cbcc
gustavog <gustavo.galazo@etec.sp.gov.br>
2022-10-31 21:45:28 -0300

    comite1

---
+++ developerspro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace developerspro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome;
+            String sobrenome;
+            Console.WriteLine("Escreva seu nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("EScreva seu sobrenome");
+            sobrenome = Console.ReadLine();
+            Console.WriteLine("Nome comleto");
+            Console.WriteLine(nome + "" + sobrenome);
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
